3a0e78312ee67aaaa5430063ce72db2dbc6677f7
Danganh6333 <134066905+Danganh6333@users.noreply.github.com>
2024-07-20 18:17:04 +0700

    AAA

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,82 @@
+/**
+ * Analogy of noun,adjectives and verbs to make the seperation of HTML,Javascript,CSS
+ * HTML represent the nouns 
+ * CSS  represent the adjectives
+ * Javascript represent the verbs
+ */
+
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas" //This is what we call declaring a value
+let first = "Matilda"
+let PI = 3.1415
+
+/**
+ * Imagine a variable as a box,so a box can hold some object and we 
+ * can write a label to describe what's in it and we can find the object when we need it 
+ * by using that label 
+ */
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+/**
+ * CONVENTION AND RULES FOR NAMING VARIABLES 
+ * 
+ * camelCase : Write the first word with a lowercase and the next words with the uppercase
+ * underscore : Write the underscore between word 
+ * 
+ * The dollar signs are allow to use in namming variable
+ * Word starting with an uppercase should be reserved for namming object,
+ * Variables that are all in uppercase are reserved for constant which will never change
+ * Make sure variable name is descriptive
+ */
+
+/**
+ * THE 7 PRIMITIVE DATA TYPES 
+ * 
+ * Number : Floating point numbers,used fpr decimals and integers
+ * String : Sequence of characters,used for text
+ * Boolean : Logical type that only be true of false,used for taking decisions
+ * Undefined : Value taken by a variable that is not yet defined('empty value'),variable declare without assigning a value
+ * Null : Also means 'empty value'
+ * Symbol (ES2015) : Value that is unique and cannot be changed
+ * BigInt (ES2020) : Larger integers than Number type can hold
+ */
+
+/**
+ * Javascript has dynamic typing : Value has type, not varribable 
+ */
+
+
+// Ctrl + / to comment 
+
+//Javascript programs are executed from top to bottom
+
+//typeof  : operator to show the type of a value
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "Gudjdmddjdojsjdsjsjo");
+
+//Dynamic typing : we can change the type of value that is hold by a varriable 
+
+//Undefined are both the value and the type
+
+//Type of null is a object
+
+/**
+ * Three different ways of declaring value in Javascript
+ * let : Can change the value of the varriable later
+ * const : It is use to declare varriables that aren't supposed to change at any point in the future,should have a initial value
+ * var : Should be avoided,can change the value of the varriable later
+ */
+
+
+
